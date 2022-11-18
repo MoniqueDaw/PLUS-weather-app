2a564b06cfc42a47e91569df805fa33e40fb428b
MoniqueDaw <ginbloess@gmail.com>
2022-11-17 22:40:06 -0500

    added another day to the forecast

--- script.js
+++ script.js
@@ -30,14 +30,13 @@ function formatDay(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
 
   let forecastHTML = `<div class="row">`;
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
-      `
-      <div class="col-2">
+      `<div class="col-2">
       <div class="weather-forecast-day">${day}</div>
             <img
               src="http://openweathermap.org/img/wn/50d@2x.png"
